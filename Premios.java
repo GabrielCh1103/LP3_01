addd9c813dbf7cd3b9e1bcecbf8d745cb2efae2d
Gabriel Condori <condorihuashuayogabriel@gmail.com>
2023-08-21 23:21:35 -0500

    hemos creado un programa de premios

---
+++ Premios.java
@@ -0,0 +1,11 @@
+
+public class Premios {
+    public static String elegir(int vida) {
+    	String gift = "";
+    	switch(vida){
+    	case 1: gift = "Te ganaste un boleto a EE.UU."; break;
+    	case 2: gift = "Un paseo a las lagunas de aqp"; break;
+    	case 3: gift = "Te ganaste 100 soles"; break;
+    	}
+    }
+}
